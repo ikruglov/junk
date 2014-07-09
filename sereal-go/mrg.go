bc30465a8c5ada7dff95ed86b372f7e1c705cdbd
Ivan Kruglov <ivan.kruglov@booking.com>
2014-07-09 23:45:13 +0200

    go fmt

--- sereal-go/mrg.go
+++ sereal-go/mrg.go
@@ -1,60 +1,60 @@
 package main
 
 import (
-    "os"
-    "fmt"
-    "time"
-    "sort"
-    "flag"
-    "io/ioutil"
-    "path/filepath"
-    "github.com/Sereal/Sereal/Go/sereal"
+	"flag"
+	"fmt"
+	"github.com/Sereal/Sereal/Go/sereal"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"sort"
+	"time"
 )
 
 type srl_file struct {
-    b []byte
-    name string
+	b    []byte
+	name string
 }
 
 func main() {
-    folder := flag.String("folder", ".", "folder to read *.srl files from")
-    flag.Parse()
+	folder := flag.String("folder", ".", "folder to read *.srl files from")
+	flag.Parse()
 
-    files, _ := filepath.Glob(*folder + "/*.srl")
-    sort.Strings(files)
+	files, _ := filepath.Glob(*folder + "/*.srl")
+	sort.Strings(files)
 
-    fmt.Fprintf(os.Stderr, "read data\n")
+	fmt.Fprintf(os.Stderr, "read data\n")
 
-    var data []srl_file
-    for _, file := range files {
-        buf, ok := ioutil.ReadFile(file)
-        if ok != nil {
-            panic("failed to read file: " + file)
-        }
+	var data []srl_file
+	for _, file := range files {
+		buf, ok := ioutil.ReadFile(file)
+		if ok != nil {
+			panic("failed to read file: " + file)
+		}
 
-        data = append(data, srl_file{buf, file})
-    }
+		data = append(data, srl_file{buf, file})
+	}
 
-    fmt.Fprintf(os.Stderr, "merge data\n")
-    t0 := time.Now()
+	fmt.Fprintf(os.Stderr, "merge data\n")
+	t0 := time.Now()
 
-    m := sereal.NewMerger()
-    m.DedupeStrings = true
-    m.KeepFlat = true
+	m := sereal.NewMerger()
+	m.DedupeStrings = true
+	m.KeepFlat = true
 
-    //m.Compression = sereal.SnappyCompressor{Incremental: true}
-    //m.Compression = sereal.ZlibCompressor{}
+	//m.Compression = sereal.SnappyCompressor{Incremental: true}
+	//m.Compression = sereal.ZlibCompressor{}
 
-    for _, srl := range data {
-        _, err := m.Append(srl.b)
-        if err != nil {
-            panic(err);
-        }
-    }
+	for _, srl := range data {
+		_, err := m.Append(srl.b)
+		if err != nil {
+			panic(err)
+		}
+	}
 
-    menc, _ := m.Finish();
-    fmt.Printf("%s", menc)
+	menc, _ := m.Finish()
+	fmt.Printf("%s", menc)
 
-    t1 := time.Now()
-    fmt.Fprintf(os.Stderr, "Merging took %v to run.\n", t1.Sub(t0))
+	t1 := time.Now()
+	fmt.Fprintf(os.Stderr, "Merging took %v to run.\n", t1.Sub(t0))
 }
--- sereal-go/psrl-mrg.go
+++ sereal-go/psrl-mrg.go
@@ -1,93 +1,93 @@
 package main
 
 import (
-    "os"
-    "fmt"
-    "time"
-    "sort"
-    "flag"
-    "runtime"
-    "io/ioutil"
-    "path/filepath"
-    "github.com/Sereal/Sereal/Go/sereal"
+	"flag"
+	"fmt"
+	"github.com/Sereal/Sereal/Go/sereal"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"runtime"
+	"sort"
+	"time"
 )
 
 func main() {
-    pcnt := flag.Int("n", 1, "workers")
-    folder := flag.String("folder", ".", "folder to read *.srl files from")
-    flag.Parse()
+	pcnt := flag.Int("n", 1, "workers")
+	folder := flag.String("folder", ".", "folder to read *.srl files from")
+	flag.Parse()
 
-    files, _ := filepath.Glob(*folder + "/*.srl")
-    sort.Strings(files)
+	files, _ := filepath.Glob(*folder + "/*.srl")
+	sort.Strings(files)
 
-    if len(files) == 0 {
-        panic("no sereal files read")
-    }
+	if len(files) == 0 {
+		panic("no sereal files read")
+	}
 
-    var data []interface{}
-    dec := sereal.NewDecoder()
+	var data []interface{}
+	dec := sereal.NewDecoder()
 
-    fmt.Fprintf(os.Stderr, "read and deserialize data\n")
-    for _, file := range files {
-        buf, ok := ioutil.ReadFile(file)
-        if ok != nil {
-            panic("failed to read file: " + file)
-        }
+	fmt.Fprintf(os.Stderr, "read and deserialize data\n")
+	for _, file := range files {
+		buf, ok := ioutil.ReadFile(file)
+		if ok != nil {
+			panic("failed to read file: " + file)
+		}
 
-        var decoded interface{}
-        if ok := dec.Unmarshal(buf, &decoded); ok != nil {
-            panic("failed to decode file: " + file)
-        }
+		var decoded interface{}
+		if ok := dec.Unmarshal(buf, &decoded); ok != nil {
+			panic("failed to decode file: " + file)
+		}
 
-        data = append(data, decoded)
-    }
+		data = append(data, decoded)
+	}
 
-    t0 := time.Now()
+	t0 := time.Now()
 
-    ln := len(data)
-    ch := make(chan []byte)
-    fmt.Fprintf(os.Stderr, "workers: %d total size: %d\n", *pcnt, ln)
+	ln := len(data)
+	ch := make(chan []byte)
+	fmt.Fprintf(os.Stderr, "workers: %d total size: %d\n", *pcnt, ln)
 
-    runtime.GOMAXPROCS(20)
+	runtime.GOMAXPROCS(20)
 
-    for i := 0; i < *pcnt; i++ {
-        from := i * (ln / *pcnt)
-        to   := (i+1) * (ln / *pcnt)
-        if i == *pcnt-1 {
-            to = len(data)
-        }
+	for i := 0; i < *pcnt; i++ {
+		from := i * (ln / *pcnt)
+		to := (i + 1) * (ln / *pcnt)
+		if i == *pcnt-1 {
+			to = len(data)
+		}
 
-        go func(slice []interface{}, from, to int) {
-            fmt.Fprintf(os.Stderr, "start worker: %d -> %d (len %d)\n", from, to, len(slice))
-            tg0 := time.Now()
-            enc := sereal.NewEncoderV3()
-            encoded, err := enc.Marshal(slice)
-            if err != nil {
-                panic("failed to marshal data")
-            }
+		go func(slice []interface{}, from, to int) {
+			fmt.Fprintf(os.Stderr, "start worker: %d -> %d (len %d)\n", from, to, len(slice))
+			tg0 := time.Now()
+			enc := sereal.NewEncoderV3()
+			encoded, err := enc.Marshal(slice)
+			if err != nil {
+				panic("failed to marshal data")
+			}
 
-            tg1 := time.Now()
-            fmt.Fprintf(os.Stderr, "finished encoding %d -> %d (%v)\n", from, to, tg1.Sub(tg0))
-            ch <- encoded
-        }(data[from:to], from, to)
-    }
+			tg1 := time.Now()
+			fmt.Fprintf(os.Stderr, "finished encoding %d -> %d (%v)\n", from, to, tg1.Sub(tg0))
+			ch <- encoded
+		}(data[from:to], from, to)
+	}
 
-    mrg := sereal.NewMerger()
-    mrg.TopLevelElement = sereal.TopLevelArray
-    mrg.KeepFlat = true
+	mrg := sereal.NewMerger()
+	mrg.TopLevelElement = sereal.TopLevelArray
+	mrg.KeepFlat = true
 
-    for i := 0; i < *pcnt; i++ {
-        if _, err := mrg.Append(<-ch); err != nil {
-            panic("failed to append to merger")
-        }
-    }
+	for i := 0; i < *pcnt; i++ {
+		if _, err := mrg.Append(<-ch); err != nil {
+			panic("failed to append to merger")
+		}
+	}
 
-    merged, err := mrg.Finish()
-    if err != nil {
-        panic("failed to get result from merger")
-    }
+	merged, err := mrg.Finish()
+	if err != nil {
+		panic("failed to get result from merger")
+	}
 
-    t1 := time.Now()
-    fmt.Printf("%s", merged)
-    fmt.Fprintf(os.Stderr, "The call took %v to run.\n", t1.Sub(t0))
+	t1 := time.Now()
+	fmt.Printf("%s", merged)
+	fmt.Fprintf(os.Stderr, "The call took %v to run.\n", t1.Sub(t0))
 }
