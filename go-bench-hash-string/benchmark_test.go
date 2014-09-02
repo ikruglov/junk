9e12d4203c640f1fccf8b0434555eb2b7d343d77
Ivan Kruglov <ivan.kruglov@booking.com>
2014-09-02 22:15:41 +0200

    improve go-bench-hash-string

--- go-bench-hash-string/benchmark_test.go
+++ go-bench-hash-string/benchmark_test.go
@@ -1,34 +1,37 @@
 package benchmark_test
 
 import (
-    "testing"
-    "encoding/binary"
+	"encoding/binary"
+	"testing"
 )
 
 var buf []byte
-func intToSlice(i int) []byte {
-    if buf == nil {
-        buf = make([]byte, 4, 4)
-    }
 
-    binary.LittleEndian.PutUint32(buf, uint32(i))
-    return buf
+func intToSlice(i int) []byte {
+	if buf == nil {
+		buf = make([]byte, 2048)
+	}
+
+	binary.LittleEndian.PutUint32(buf, uint32(i))
+	return buf
 }
 
 func BenchmarkConvertToStringOutside(b *testing.B) {
-    var str string
-    m := make(map[string]interface{})
-    for i := 0; i < b.N; i++ {
-        str = string(intToSlice(i))
-        m[str] = i
-    }
+	var str string
+	m := make(map[string]interface{})
+
+	for i := 0; i < b.N; i++ {
+		str = string(intToSlice(i))
+		m[str] = i
+	}
 }
 
 func BenchmarkConvertToStringInside(b *testing.B) {
-    var slice []byte
-    m := make(map[string]interface{})
-    for i := 0; i < b.N; i++ {
-        slice = intToSlice(i)
-        m[string(slice)] = i
-    }
+	var slice []byte
+	m := make(map[string]interface{})
+
+	for i := 0; i < b.N; i++ {
+		slice = intToSlice(i)
+		m[string(slice)] = i
+	}
 }
